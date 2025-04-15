d53077fc32365168a101d995194160026d7d7625
dominikkubas06 <dominik.kubas06@gmail.com>
2025-04-15 18:55:51 +0200

    wip: remove user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -6,3 +6,9 @@ def add_user(users_data:list)->None:
     new_name:str=input("Podaj imię nowego znajomego: ")
     new_location:str=input("Podaj lokalizację: ")
     new_posts:str=input("Podaj liczbę postów nowego znajomego: ")
+
+def remove_user(users_data:list)->None:
+    user_name:str=input("Podaj imię znajomego do usunięcia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
